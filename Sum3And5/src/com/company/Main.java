17ee39da45f11db49d11692d5a24195e84d027f5
tanmaythaware <85193272+tanmaythaware@users.noreply.github.com>
2021-06-09 01:53:31 +0530

    Add files via upload

---
+++ Sum3And5/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        for(int i = 1; i <= 1000; i++){
+            if(i%3==0 && i%5==0){
+                sum = sum + i;
+                count = count + 1;
+                System.out.println("Number Found : " +i);
+            }
+            if(count == 5){
+                break;
+            }
+        }
+        System.out.println("Sum : " + sum);
+
+    }
+}
